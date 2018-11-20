abec278f6bee633c2751af20dd3f0c9f78c7bad7
khoursaaf <khoursa@gmail.com>
2018-11-20 16:27:00 +0100

    correction

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-        <p>En allant de 1 à 100 avec un pas de 15, afficher le message On tient le bon bout.</p>
-        <?php
-        for ($a=1; $a <= 100; $a += 15) {
-           echo $a.' - On tient le bon bout'.'<br />';
-        }
-         ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>En allant de 1 à 100 avec un pas de 15, afficher le message "On tient le bon bout".</p>
+    <?php for ($a=1; $a <= 100; $a += 15) { ?>
+    <p><?php echo $a.' '.'On tient le bon bout' ; } ?>
+  </body>
 </html>
